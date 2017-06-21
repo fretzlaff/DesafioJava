c474416eddeb1f60af4296f1ca430d43976ba332
Fabio Retzlaff <fabio.retzlaff@neogrid.com>
2017-06-21 15:36:27 -0300

    Testes

--- src/test/java/br/desafio/contact/ContactTest.java
+++ src/test/java/br/desafio/contact/ContactTest.java
@@ -1,31 +1,72 @@
 package br.desafio.contact;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import br.desafio.controller.ContactsController;
+import br.desafio.controller.validation.ContactValidator;
+import br.desafio.model.Contact;
+import br.desafio.repos.ContactsRepos;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@WebMvcTest(controllers = ContactsController.class)
 public class ContactTest {
 
-	@LocalServerPort
-	private int port;
+    @Autowired
+	private MockMvc mockMvc;
 
-	@Autowired
-	private TestRestTemplate restTemplate;
+    @MockBean
+    private ContactsRepos contactsRepos;
+
+    @MockBean
+    private ContactValidator contactValidator;
+
+	@Before
+	public void setup() throws Exception {
+		when(contactValidator.supports(any())).thenReturn(true);
+	}
 
 	@Test
-	public void exampleTest() {
-		final String body = restTemplate.getForObject("/", String.class);
-		System.out.println("body: " + body);
-		assertThat(body).contains("Hello World");
+	public void testContactList() throws Exception {
+		this.mockMvc.perform(get("/contact/"))
+			.andExpect(status().isOk())
+			.andExpect(view().name("contact/list"))
+			.andExpect(content().string(Matchers.containsString("/contact/add/")))
+			.andDo(print());
+	}
+
+	@Test
+	public void testContactSave() throws Exception {
+
+		this.mockMvc.perform(post("/contact/save")
+					.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+					.param("name", "joao")
+		            .param("email", "joao@empresa.com")
+		            .param("age", "22")
+		            .param("state", "SC")
+		            .param("role", "Programador")
+					.sessionAttr("contact", new Contact()))
+			.andExpect(status().isOk())
+			.andExpect(view().name("contact/list"))
+			.andExpect(content().string(Matchers.containsString("/contact/add/")))
+			.andDo(print());
 	}
 
 }
--- src/test/java/br/desafio/segmentation/SegmentationTest.java
+++ src/test/java/br/desafio/segmentation/SegmentationTest.java
@@ -1,31 +1,78 @@
 package br.desafio.segmentation;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import br.desafio.controller.ContactsController;
+import br.desafio.controller.validation.SegmentationValidator;
+import br.desafio.model.Contact;
+import br.desafio.repos.SegmentationRepos;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@WebMvcTest(controllers = ContactsController.class)
 public class SegmentationTest {
 
-	@LocalServerPort
-	private int port;
+    @Autowired
+	private MockMvc mockMvc;
 
-	@Autowired
-	private TestRestTemplate restTemplate;
+    @MockBean
+    private SegmentationRepos segmentationRepos;
+
+    @MockBean
+    private SegmentationValidator segmentationValidator;
+
+	@Before
+	public void setup() throws Exception {
+		when(segmentationValidator.supports(any())).thenReturn(true);
+	}
 
 	@Test
-	public void exampleTest() {
-		final String body = restTemplate.getForObject("/", String.class);
-		System.out.println("body: " + body);
-		assertThat(body).contains("Hello World");
+	public void testSegmentationList() throws Exception {
+		this.mockMvc.perform(get("/segmentation/"))
+			.andExpect(status().isOk())
+			.andExpect(view().name("segmentation/list"))
+			.andExpect(content().string(Matchers.containsString("/segmentation/add/")))
+			.andDo(print());
+	}
+
+	@Test
+	public void testSegmentationSave() throws Exception {
+
+		/*
+		 	searchParams[0].needsCombinator
+			searchParams[0].contactAttribute
+			searchParams[0].comparisonRule
+			searchParams[0].value
+			searchParams[1].groupCombinator
+
+			verificar o nome dos objetos na httpsession
+		 */
+		this.mockMvc.perform(post("/segmentation/save")
+					.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+					.param("description", "segmentation")
+		            .param("role", "Programador")
+					.sessionAttr("contact", new Contact()))
+			.andExpect(status().isOk())
+			.andExpect(view().name("contact/list"))
+			.andExpect(content().string(Matchers.containsString("/contact/add/")))
+			.andDo(print());
 	}
 
 }
